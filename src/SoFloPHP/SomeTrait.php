0e43054e1ed520ae27aeaa9ce6dacbfaf2893e41
Michael Moussa <michael.moussa@gmail.com>
2014-05-20 21:04:58 -0400

    "mocking" php functions and cheating randomness

--- src/SoFloPHP/SomeTrait.php
+++ src/SoFloPHP/SomeTrait.php
@@ -5,4 +5,14 @@ namespace SoFloPHP;
 trait SomeTrait
 {
     public $foo = 'bar';
+
+    public function whatTimeIsIt()
+    {
+        return microtime();
+    }
+
+    public function getRandomNumber($min, $max)
+    {
+        return rand($min, $max);
+    }
 }
--- test/SoFloPHP/TipsTricksUsefulStuffTest.php
+++ test/SoFloPHP/TipsTricksUsefulStuffTest.php
@@ -1,53 +1,75 @@
 <?php
 
-namespace SoFloPHP;
+namespace SoFloPHP {
 
-use Xpmock\TestCase;
+    use Xpmock\TestCase;
 
-class TipsTricksUsefulStuffTest extends TestCase
-{
-    public function testPersistInsertsNewAccountsIntoTheDatabaseMockedVersion()
+    class TipsTricksUsefulStuffTest extends TestCase
     {
-        $stmt = $this->mock('stdClass')
-            ->execute([500], $this->once())
-            ->new();
+        public function testPersistInsertsNewAccountsIntoTheDatabaseMockedVersion()
+        {
+            $stmt = $this->mock('stdClass')
+                ->execute([500], $this->once())
+                ->new();
 
-        $pdo = $this->mock('PDO')
-            ->prepare(['insert into account (balance) values (?)'], $stmt, $this->once())
-            ->lastInsertId(1, $this->once())
-            ->new('sqlite::memory:');
-        $service = new BankAccountService($pdo);
+            $pdo = $this->mock('PDO')
+                ->prepare(['insert into account (balance) values (?)'], $stmt, $this->once())
+                ->lastInsertId(1, $this->once())
+                ->new('sqlite::memory:');
+            $service = new BankAccountService($pdo);
 
-        $account = new BankAccount();
-        $account->deposit(500);
-        $newAccountId = $service->persist($account);
-        $this->assertEquals(1, $newAccountId);
+            $account = new BankAccount();
+            $account->deposit(500);
+            $newAccountId = $service->persist($account);
+            $this->assertEquals(1, $newAccountId);
+        }
+
+        /**
+         * @dataProvider explodingStringProvider
+         */
+        public function testExplodingString($string, $delimiter, $expected)
+        {
+            $this->assertSame($expected, explode($delimiter, $string));
+        }
+
+        public function explodingStringProvider()
+        {
+            return [
+                ['foo,bar', ',', ['foo', 'bar']],
+                ['oh|hello|there|everyone', '|', ['oh', 'hello', 'there', 'everyone']],
+            ];
+        }
+
+        public function testFooIsBarIfClassUsesSomeTrait()
+        {
+            $someTraitTester = new SomeTraitTester();
+            $this->assertSame('bar', $someTraitTester->foo);
+        }
+
+        public function testMockingPHPFunctions()
+        {
+            $someTraitTester = new SomeTraitTester();
+            $this->assertSame(microtime(), $someTraitTester->whatTimeIsIt());
+        }
+
+        public function testRandomNumberIsRandom()
+        {
+            srand(123); // the first call to rand(1, 100) will always return "7"
+            $someTraitTester = new SomeTraitTester();
+            $this->assertSame(7, $someTraitTester->getRandomNumber(1, 100));
+        }
+    }
+
+    class SomeTraitTester
+    {
+        use SomeTrait;
     }
 
     /**
-     * @dataProvider explodingStringProvider
+     * Stubs out the microtime() function. Test will fail if this function is removed.
      */
-    public function testExplodingString($string, $delimiter, $expected)
+    function microtime()
     {
-        $this->assertSame($expected, explode($delimiter, $string));
+        return 'it works!';
     }
-
-    public function explodingStringProvider()
-    {
-        return [
-            ['foo,bar', ',', ['foo', 'bar']],
-            ['oh|hello|there|everyone', '|', ['oh', 'hello', 'there', 'everyone']],
-        ];
-    }
-
-    public function testFooIsBarIfClassUsesSomeTrait()
-    {
-        $someTraitTester = new SomeTraitTester();
-        $this->assertSame('bar', $someTraitTester->foo);
-    }
-}
-
-class SomeTraitTester
-{
-    use SomeTrait;
 }
