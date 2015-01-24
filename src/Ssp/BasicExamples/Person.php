0bc62c677c703debe43458504bc1d564381c0937
Michael Moussa <michael.moussa@gmail.com>
2015-01-24 12:39:14 -0500

    Adding support for middle initial (Broken!)
    This breaks the first test because middleInitial is a new constructor
    parameter that the other test does not provide.

--- src/Ssp/BasicExamples/Person.php
+++ src/Ssp/BasicExamples/Person.php
@@ -15,24 +15,31 @@ class Person
     protected $lastName;
 
     /**
+     * @var string
+     */
+    protected $middleInitial;
+
+    /**
      * Constructor
      *
      * @param string $firstName
      * @param string $lastName
+     * @param string $middleInitial
      */
-    public function __construct($firstName, $lastName)
+    public function __construct($firstName, $lastName, $middleInitial)
     {
         $this->firstName = $firstName;
         $this->lastName = $lastName;
+        $this->middleInitial = $middleInitial;
     }
 
     /**
-     * Returns the first and last names separated by a space.
+     * Returns the first name, middle initial, and last name separated by spaces.
      *
      * @return string
      */
     public function getFullName()
     {
-        return $this->firstName . ' ' . $this->lastName;
+        return $this->firstName . ' ' . $this->middleInitial . '. ' . $this->lastName;
     }
 }
--- test/Ssp/BasicExamples/PersonTest.php
+++ test/Ssp/BasicExamples/PersonTest.php
@@ -6,7 +6,14 @@ class PersonTest extends \PHPUnit_Framework_TestCase
 {
     public function testFullNameConsistsOfFirstAndLastName()
     {
+        // Adding the required $middleInitial argument to the constructor breaks this test.
         $person = new Person('Michael', 'Moussa');
         $this->assertSame('Michael Moussa', $person->getFullName());
+    }
+
+    public function testFullNameIncludesMiddleInitial()
+    {
+        $person = new Person('Michael', 'Moussa', 'P');
+        $this->assertSame('Michael P. Moussa', $person->getFullName());
     }
 }
