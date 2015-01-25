e4055ada2238c7be8af0dd0d637ebb947eeb05d7
Michael Moussa <michael.moussa@gmail.com>
2015-01-24 20:02:08 -0500

    Weather Service example with mocked cache test

---
+++ test/Ssp/TestingInIsolation/WeatherServiceTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Ssp\TestingInIsolation;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Stream\Stream;
+use Mockery as m;
+
+class WeatherServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanGetTemperatureReallySlowlyAndBrittly()
+    {
+        $this->markTestSkipped('<--- Comment out this line to watch this test fail gloriously!');
+
+        // If we're testing against a real API, we have no way to know what value to expect.
+        $expectedTemperature = 'How am I supposed to know?';
+
+        // Real cache and real HTTP client means our test fails if either is offline... even if the code is correct!
+        $weatherService = new WeatherService(new \Memcached(), new Client());
+        $currentTemperature = $weatherService->getTemperature('Miami, FL');
+
+        $this->assertSame($expectedTemperature, $currentTemperature);
+    }
+
+    public function testReturnsTemperatureIfFoundInCache()
+    {
+        $expectedTemperature = '90F';
+
+        // The cache will behave exactly as we told it, and complain if anything unexpected happens.
+        $cache = $this->getMockBuilder('Memcached')
+            ->setMethods(['get'])
+            ->getMock();
+        $cache->expects($this->once())
+            ->method('get')
+            ->with(md5('Miami, FL'))
+            ->will($this->returnValue(['temperature' => $expectedTemperature]));
+
+        $weatherService = new WeatherService($cache, new Client());
+        $currentTemperature = $weatherService->getTemperature('Miami, FL');
+
+        $this->assertSame($expectedTemperature, $currentTemperature);
+    }
+
+    public function testRetrievesTemperatureFromApiIfNotFoundInCache()
+    {
+        $expectedTemperature = '90F';
+        $expectedCacheKey = md5('Miami, FL');
+        $temperatureData = ['temperature' => '90F'];
+
+        // Use Mockery for our mocks this time.
+        $cache = m::mock('Memcached');
+        $cache->shouldReceive('get')
+            ->once()
+            ->with($expectedCacheKey)
+            ->andReturn(null);
+        $cache->shouldReceive('set')
+            ->once()
+            ->with($expectedCacheKey, $temperatureData, 900);
+        $httpClient = m::mock('GuzzleHttp\Client');
+        $httpClient->shouldReceive('get')
+            ->once()
+            ->with('https://some-weather-api.com/temperature/' . urlencode('Miami, FL'))
+            ->andReturn(new Response(200, [], Stream::factory(json_encode($temperatureData))));
+
+        $weatherService = new WeatherService($cache, $httpClient);
+        $currentTemperature = $weatherService->getTemperature('Miami, FL');
+
+        $this->assertSame($expectedTemperature, $currentTemperature);
+
+        /*
+         * We need this so that Mockery will verify that all mocked expectations have been met.
+         * There's a trick to avoid needing to do this, which we'll see later.
+         */
+        m::close();
+    }
+}
