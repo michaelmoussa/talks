6e19cfdca1d9c9c7c26ffd0dcea98c82bd549888
Michael Moussa <michael.moussa@gmail.com>
2015-01-27 00:03:49 -0500

    "Mock" global function with uopz extension

--- test/Ssp/TipsAndTricks/MockingFunctionsHelperTest.php
+++ test/Ssp/TipsAndTricks/MockingFunctionsHelperTest.php
@@ -6,16 +6,29 @@ class MockingFunctionsHelperTest extends \PHPUnit_Framework_TestCase
 {
     public function testMockingPHPFunctions()
     {
+        // Or, we can use the fancy UOPZ extension!
+
+        uopz_backup('microtime'); // backup "microtime" before we override it, so we don't affect other tests
+
         /*
-         * Now it will work, since we've overridden the microtime() function
-         * under this namespace to return a constant value.
+         * Replace "microtime" to return a simple constant
          */
+        uopz_function('microtime', function () {
+            return 12345;
+        });
+
+        // Run the test
         $helper = new MockingFunctionsHelper();
         $this->assertSame(12345, $helper->whatMicrotimeTimeIsIt());
-    }
-}
 
-function microtime($getAsFloat = false)
-{
-    return 12345;
+        // Restore the original "microtime" function
+        uopz_restore('microtime');
+
+        /*
+         * Read more about UOPZ at the links below:
+         *
+         * http://php.net/manual/en/ref.uopz.php
+         * http://blog.krakjoe.ninja/2015/01/mocking-php.html
+         */
+    }
 }
