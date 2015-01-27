76296526570eba553af3c260c51626f4942d06cd
Michael Moussa <michael.moussa@gmail.com>
2015-01-26 20:13:59 -0500

    Sample of 100% coverage not covering everything

--- test/Ssp/CodeCoverage/CalculatorTest.php
+++ test/Ssp/CodeCoverage/CalculatorTest.php
@@ -9,4 +9,14 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
         $calculator = new Calculator();
         $this->assertSame(2.5, $calculator->divide(5, 2));
     }
+
+    public function testCanBreakTheDivideFunctionBecauseForgotToAccountForDividingByZero()
+    {
+        $this->markTestSkipped('<--- Comment out this line to watch this test fail gloriously!');
+
+        $calculator = new Calculator();
+
+        // Yes, we have "100%" test coverage, but that doesn't mean we've tested everything!
+        $calculator->divide(5, 0);
+    }
 }
