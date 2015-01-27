64a65835ba1ec20dccdf96ccfd953b6edc496228
Michael Moussa <michael.moussa@gmail.com>
2015-01-26 20:15:55 -0500

    Account for division by zero, but reducing test coverage

--- src/Ssp/CodeCoverage/Calculator.php
+++ src/Ssp/CodeCoverage/Calculator.php
@@ -6,6 +6,9 @@ class Calculator
 {
     public function divide($numerator, $denominator)
     {
+        if ($denominator == 0) {
+            throw new \InvalidArgumentException('Cannot divide by zero!');
+        }
         return $numerator / $denominator;
     }
 }
