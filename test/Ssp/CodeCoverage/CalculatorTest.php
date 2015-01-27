d6c5322ac19a5c13cda9cd0ea79f622cd4f21428
Michael Moussa <michael.moussa@gmail.com>
2015-01-26 20:17:06 -0500

    Back to 100% coverage

--- test/Ssp/CodeCoverage/CalculatorTest.php
+++ test/Ssp/CodeCoverage/CalculatorTest.php
@@ -9,4 +9,14 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
         $calculator = new Calculator();
         $this->assertSame(2.5, $calculator->divide(5, 2));
     }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Cannot divide by zero!
+     */
+    public function testThrowsInvalidArgumentExceptionIfTryingToDivideByZero()
+    {
+        $calculator = new Calculator();
+        $calculator->divide(5, 0);
+    }
 }
