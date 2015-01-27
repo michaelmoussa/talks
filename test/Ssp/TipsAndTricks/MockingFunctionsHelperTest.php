7769bee6967d1df31b001cc30696322cd8d4ae6e
Michael Moussa <michael.moussa@gmail.com>
2015-01-26 23:53:37 -0500

    Fixed test by "mocking" microtime()

--- test/Ssp/TipsAndTricks/MockingFunctionsHelperTest.php
+++ test/Ssp/TipsAndTricks/MockingFunctionsHelperTest.php
@@ -7,12 +7,15 @@ class MockingFunctionsHelperTest extends \PHPUnit_Framework_TestCase
     public function testMockingPHPFunctions()
     {
         /*
-         * This test fails because the return value of microtime() is constantly changing.
-         *
-         * Without mocking this in some way, you won't be able to make any time-related
-         * assertions against your system under test if it uses microtime().
+         * Now it will work, since we've overridden the microtime() function
+         * under this namespace to return a constant value.
          */
         $helper = new MockingFunctionsHelper();
-        $this->assertSame(microtime(), $helper->whatMicrotimeTimeIsIt());
+        $this->assertSame(12345, $helper->whatMicrotimeTimeIsIt());
     }
+}
+
+function microtime($getAsFloat = false)
+{
+    return 12345;
 }
